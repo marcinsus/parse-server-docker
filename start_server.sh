095ca9440ef0d69f003a15fe17eee7c439bce7cf
Peter Willemsen <peter@codebuffet.co>
2016-04-11 19:24:45 +0200

    oops

--- start_server.sh
+++ start_server.sh
@@ -3,7 +3,7 @@ if [ -z "$MONGO_URL" ]; then
 	export MONGO_URL='mongodb://localhost:27017/dev'
 	echo "No Mongo URL set, auto-making one..."
 	/usr/bin/mongod --config /etc/mongodb.conf &
-else
+fi
 cd /usr/src/app
 npm start &
 bash start_dashboard.sh
